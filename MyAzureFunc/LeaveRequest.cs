50b4e8d4d88515734048faf293f841f543ca7f6e
Sarah Mae Macaraeg <sarahmaemacaraeg@outlook.com>
2020-02-10 11:02:59 -0800

    added a leaverequest class

---
+++ MyAzureFunc/LeaveRequest.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAzureFunc
+{
+    class LeaveRequest
+    {
+        [JsonProperty(PropertyName = "employeeID")]
+        public string EmployeeId { get; set; }
+        public string Name { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+    }
+}
