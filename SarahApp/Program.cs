e6e2a9686020deac3781b42812f262fed37c69f5
Sarah Mae Macaraeg <sarahmaemacaraeg@outlook.com>
2020-02-26 10:20:25 -0800

    Sending messages using ServiceBus

---
+++ SarahApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace SarahSenderApp
+{
+    class Program
+    {
+        const string ServiceBussConnectionString = "Endpoint=sb://sarahservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=mSzpVDuDSN0tHF3qZsY3KS1h54ZMNpc1B3FS/ShIfX0=";
+        const string QueueName = "sarahqueue";
+        static IQueueClient queueClient;
+        public static async Task Main(string[] args)
+        {
+            const int numberOfMessages = 10;
+            queueClient = new QueueClient(ServiceBussConnectionString, QueueName);
+            Console.WriteLine("====================================================");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
